67af34521761c522a4d10498db9544a9111de409
Mariano RA <rodriguezarce.mariano@gmail.com>
2023-09-26 13:50:58 -0300

    -501

--- dott-front/src/app/api/nest/dolar/route.js
+++ dott-front/src/app/api/nest/dolar/route.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 import { apiUrl } from "../utils/utils";
 import { NextResponse } from "next/server";
-
+import {
+  withApiAuthRequired,
+  getAccessToken,
+  getSession,
+} from "@auth0/nextjs-auth0";
 import https from "https";
 const fs = require("fs");
 
@@ -11,27 +15,59 @@ const fs = require("fs");
 // });
 
 export async function POST(request) {
-  const { headers } = await request;
-  const precioDolar = await request.json();
+  // const { headers } = await request;
+  // const precioDolar = await request.json();
 
-  const accessToken = headers.get("authorization");
+  // const accessToken = headers.get("authorization");
 
-  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-  let response = await axios
-    .post(`${apiUrl}/api/dolar`, {
-      // httpsAgent: httpsAgent,
-      headers: {
-        Authorization: accessToken,
+  // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+  // let response = await axios
+  //   .post(`${apiUrl}/api/dolar`, {
+  //     // httpsAgent: httpsAgent,
+  //     headers: {
+  //       Authorization: accessToken,
+  //     },
+  //     body: precioDolar,
+  //   })
+  //   .then((response) => {
+  //     return response.data;
+  //   })
+  //   .catch((err) => {
+  //     return err.response.data;
+  //   });
+  // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
+
+  // return NextResponse.json({ response });
+
+  try {
+    const { accessToken } = await getSession(request, null, {
+      authorizationParams: {
+        scope: "create:tablas offline_access",
       },
-      body: precioDolar,
-    })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      return err.response.data;
     });
-  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
 
-  return NextResponse.json({ response });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    const precioDolar = await request.json();
+
+    // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+    const response = await axios.post(
+      `${apiUrl}/api/dolar`,
+      JSON.stringify(precioDolar),
+      config
+    );
+
+    console.log(response.data);
+
+    return NextResponse.json({ response: response.data });
+  } catch (error) {
+    console.error("Error en la solicitud POST:", error);
+
+    return NextResponse.error("Error en la solicitud POST", 500);
+  }
 }
