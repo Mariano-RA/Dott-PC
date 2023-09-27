8b0e4948e69853b2ea5553bde0d2f3bf1ab3324c
Mariano RA <rodriguezarce.mariano@gmail.com>
2023-09-27 12:59:53 -0300

    adasdad

--- dott-front/src/app/api/nest/products/list/route.js
+++ dott-front/src/app/api/nest/products/list/route.js
@@ -53,25 +53,15 @@ export async function POST(request) {
     const response = await axios.request(config);
     process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
 
-    const headers = {
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json", // Especifica el tipo de contenido como JSON
-    };
-
-    const config_2 = {
-      method: "post",
-      url: `${apiUrl}/api/productos`,
-      data: response.data,
-      headers: headers,
-    };
-
-    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+    // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
     const resVal = await axios.post(`${apiUrl}/api/productos`, response.data, {
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json", // Especifica el tipo de contenido como JSON
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json", // Especifica el tipo de contenido como JSON
+      },
     });
 
-    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
+    // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
 
     return NextResponse.json({ resVal: resVal });
   } catch (error) {
