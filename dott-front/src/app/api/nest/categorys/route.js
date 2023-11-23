889c537afa6089c229333a9db54eb5c0e9c87364
MarianoRA <rodriguezarce.mariano@gmail.com>
2023-11-23 11:30:36 -0300

    aaa

--- dott-front/src/app/api/nest/categorys/route.js
+++ dott-front/src/app/api/nest/categorys/route.js
@@ -4,16 +4,12 @@ import axios from "axios";
 
 export async function GET() {
   process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-  let categorys;
-  try {
-    categorys = await axios
+  const categorys = await axios
     .get(`${apiUrl}/productos/categorias`)
     .then((response) => {
       return response.data;
     });
-  } catch (error) {
-    console.log(categorys);
-  }
-    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
+  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
+
   return NextResponse.json({ categorys });
 }
