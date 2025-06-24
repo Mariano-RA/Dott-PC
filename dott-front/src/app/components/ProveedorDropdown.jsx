845bc20b28cf64599e7d6a1e1728a18034190c8b
Mariano-RA <rodriguezarce.mariano@gmail.com>
2025-06-23 21:56:08 -0300

    asdasdasd

--- dott-front/src/app/components/ProveedorDropdown.jsx
+++ dott-front/src/app/components/ProveedorDropdown.jsx
@@ -26,11 +26,18 @@ export default function ProveedorDropdown({ handleSelectProveedor }) {
   }
 
   return (
-    <Menu as="div" className="relative inline-block text-left" style={{ width: "180px" }}>
+    <Menu
+      as="div"
+      className="relative inline-block text-left"
+      style={{ width: "140px" }}
+    >
       <div>
         <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-red-950 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           {selectedProveedor.value}
-          <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+          <ChevronDownIcon
+            className="-mr-1 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
         </Menu.Button>
       </div>
 
--- dott-front/src/app/products/category/[id]/page.jsx
+++ dott-front/src/app/products/category/[id]/page.jsx
@@ -140,7 +140,7 @@ const Page = ({ params }) => {
                     <Squares2X2Icon className="h-5 w-5" aria-hidden="true" />
                   </button>
                 </div>
-                <Dropdown className="me-2" handleSort={handleSelectedSort}/>
+                <Dropdown handleSort={handleSelectedSort}/>
                 <ProveedorDropdown
                   handleSelectProveedor={handleSelectProveedor}
                 />
